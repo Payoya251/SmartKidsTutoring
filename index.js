aa31481adefb741ef435ed1d7d05afd6938aa06d
Payoya251 <anthonyventura2324@gmail.com>
2025-04-24 14:07:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const https = require('https');
 
-// Use the port provided by Render or fallback to 3000 for local development
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World from SmartKidsTutoring!');
 });
 
-// Add the health check endpoint
 app.get('/health', (req, res) => {
-  res.sendStatus(200); // Send a 200 OK status
+  res.status(200).json({ status: 'OK' });
 });
-
-//app.listen(port, '0.0.0.0', () => {
-//  console.log(`App listening on port ${port}`);
-//});
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Listening at http://0.0.0.0:${port}`);
+  console.log(`App listening on port ${port}`);
 });
-
-
-//const options = {
-//  key: fs.readFileSync('path/to/private-key.pem'),
-//  cert: fs.readFileSync('path/to/certificate.pem')
-//};
-
-//https.createServer(options, app).listen(port, '0.0.0.0', () => {
-//  console.log(`Secure server running on port ${port}`);
-//});

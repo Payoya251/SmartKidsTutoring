fc850e639aab717b3736fd23c64501bb656d8fad
Payoya251 <anthonyventura2324@gmail.com>
2025-04-24 13:54:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const https = require('https');
 
 // Use the port provided by Render or fallback to 3000 for local development
 const port = process.env.PORT || 3000;
@@ -17,6 +19,16 @@ app.get('/health', (req, res) => {
 //  console.log(`App listening on port ${port}`);
 //});
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Listening at http://0.0.0.0:${port}`);
+//app.listen(port, '0.0.0.0', () => {
+//  console.log(`Listening at http://0.0.0.0:${port}`);
+//});
+
+
+const options = {
+  key: fs.readFileSync('path/to/private-key.pem'),
+  cert: fs.readFileSync('path/to/certificate.pem')
+};
+
+https.createServer(options, app).listen(port, '0.0.0.0', () => {
+  console.log(`Secure server running on port ${port}`);
 });

60812b055b337851b8c8941de45f79e87e640715
Payoya251 <anthonyventura2324@gmail.com>
2025-04-24 21:36:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* ver 1.0
 const express = require('express');
 const app = express();
 
@@ -16,4 +17,28 @@ app.get('/health', (req, res) => {
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening on port ${port}`);
+});
+*/
+
+
+// ver 1.1
+const express = require('express');
+const path = require('path'); // Added this line
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from SmartKidsTutoring!');
+});
+
+// Use folder Frontend to use the html code from there
+app.use(express.static(path.join(__dirname, 'Frontend')));
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK' });
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`App listening on port ${port}`);
 });

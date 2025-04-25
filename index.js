e632fed5f84724811e99b243dbca1076184cdf2e
Payoya251 <anthonyventura2324@gmail.com>
2025-04-25 03:10:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.listen(port, '0.0.0.0', () => {
 });
 
 
+/*
 //NEW CODE
 console.log("MongoDB URI:", process.env.MONGODB_URI);
 // check mongodb connection
@@ -41,3 +42,34 @@ async function connectDB() {
   }
 }
 connectDB();
+*/
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://anthonyventura2324:36kgQwCf6zqWEiDa@smartkidstutoring.jahng0c.mongodb.net/?retryWrites=true&w=majority&appName=SmartKidsTutoring";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
